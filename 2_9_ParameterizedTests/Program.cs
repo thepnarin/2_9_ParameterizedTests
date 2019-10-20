cc62d7c703d25350b60ad8c0e14a40b1593623ee
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:37:03 +0700

    ParameterizedTests

---
+++ 2_9_ParameterizedTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_9_ParameterizedTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
